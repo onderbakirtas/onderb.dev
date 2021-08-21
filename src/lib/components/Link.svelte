25eee345b437a64a184e76cd02c21130dfd5b146
Onder Bakirtas <onderbakirtas@gmail.com>
2021-08-21 19:31:56 +0300

    feat: restyled links to have better visibility

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -9,9 +9,28 @@
   a {
     color: #012;
     text-decoration: none;
+    display: inline-block;
+    position: relative;
+    transition: 0.25s;
+  }
+
+  a::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 3px;
+    width: 100%;
+    height: 2px;
+    background: #012;
+    transition: 0.15s;
+    z-index: -1;
   }
 
   a:hover {
-    text-decoration: underline;
+    color: #eef;
+  }
+
+  a:hover::after {
+    height: calc(100% - 4px);
   }
 </style>
