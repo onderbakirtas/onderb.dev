312f429692161bda0772c257c461be435935f145
Onder Bakirtas <onderbakirtas@gmail.com>
2021-08-21 19:53:28 +0300

    feat: merged some styles, removed unnecessary ones in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,36 +1,37 @@
 <script>
-  import { quotes } from '$lib/data/quotes'
+	import { quotes } from '$lib/data/quotes';
 
-  const randomizer = parseInt(Math.random() * quotes.length)
+	const randomizer = parseInt(Math.random() * quotes.length);
 
-  const quote = quotes[randomizer]
+	const quote = quotes[randomizer];
 </script>
 
 <footer>
-  <q>{quote.body}</q>
-  <cite>– {quote.author}</cite>
+	<q>{quote.body}</q>
+	<cite>– {quote.author}</cite>
 </footer>
 
 <style>
-  footer {
-    max-width: 40rem;
-    margin: auto auto 0;
-    margin-top: min(2rem, auto);
-    padding: 1rem 0;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-  }
-  q {
-    color: #678;
-    font-family: serif;
-    font-size: clamp(1rem, 2vw, 1.125rem);
-  }
+	footer {
+		max-width: 40rem;
+		margin: auto auto 0;
+		margin-top: min(2rem, auto);
+		padding: 1rem 0;
+		display: flex;
+		flex-direction: column;
+	}
 
-  cite {
-    font-size: 1.125rem;
-    margin-top: 0.5rem;
-    display: inline-block;
-    align-self: flex-end;
-  }
+	q,
+	cite {
+		font-size: clamp(1rem, 2vw, 1.125rem);
+	}
+	q {
+		color: #678;
+		font-family: serif;
+	}
+
+	cite {
+		margin-top: 0.5rem;
+		align-self: flex-end;
+	}
 </style>
